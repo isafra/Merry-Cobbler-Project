fdbe51d7991f40d04367a167e5abb9924d009e96
Isabella Franca <isafra_89@icloud.com>
2025-06-23 18:34:19 +0100

    fix to index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://isafra.github.io',
+    site: 'https://isafra.github.io/Merry-Cobbler-Project',
     base: '/Merry-Cobbler-Project/',
 });
